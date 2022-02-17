7bb0ec6738f9035778352721378230368fde9b2d
RiyadMia <96831477+RiyadMia@users.noreply.github.com>
2022-02-17 16:19:33 +0600

    last addd file

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-function myIncome() {
+
+const myIncome = document.getElementById('total').addEventListener('click', function () {
     const foot = parseInt(document.getElementById('foot').value);
     const rent = parseInt(document.getElementById('rent').value);
     const clothes = parseInt(document.getElementById('clothes').value);
-    const getResult = document.getElementById('result').innerHTML = foot + rent + clothes;
+    const getResult = document.getElementById('result').innerHTML = foot +
+        rent + clothes;
     const salare = parseInt(document.getElementById('salare').value);
     document.getElementById('balance').innerHTML = salare - getResult;
-};
+
+    // erro massage
+
+    const succesed = document.getElementById('enoght');
+    const erro = document.getElementById('not-enoght');
+    if (getResult < salare) {
+        succesed.style.display = 'block'
+        erro.style.display = 'none'
+    }
+    else {
+        succesed.style.display = 'none'
+        erro.style.display = 'block'
+    }
+})
+
 
 // savaing amount
 
@@ -15,5 +31,3 @@ function myIncomeSave() {
     const percantis = parseInt(document.getElementById('percantis').value);
     document.getElementById('save-amount').innerHTML = save * percantis;
 }
-
-
